e33cc4fec3dbafd14e6ee24a538618bd1e8dc0c9
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-01-14 20:56:05 -0300

    Alterando organização e começando capitulo 4 do livro

---
+++ Capitulo05/Conteudo/Exemplo01.c
@@ -0,0 +1,13 @@
+// Laço de repetição while
+
+#include <stdio.h>
+
+int main(void){
+    int contagem = 100;
+
+    while (contagem > 0){
+        contagem--;
+        printf("Contagem: %d\n", contagem);
+    }
+    
+}
--- modificador.py
+++ modificador.py
@@ -1,3 +1,8 @@
+"""
+    Programa que realiza a modificação de nome de arquivos. Com base na localização da pasta desse arquivo
+    o nome inicial e o nome que você deseja passar como argumento.
+"""
+
 from pathlib import Path
 
 def renomear_arquivos(pasta):
@@ -8,10 +13,10 @@ def renomear_arquivos(pasta):
         return
 
     for arquivo in pasta.iterdir():
-        if arquivo.is_file() and arquivo.name.startswith("Atividade") and arquivo.suffix == ".c":
+        if arquivo.is_file() and arquivo.name.startswith("Exemplo") and arquivo.suffix == ".c":
             try:
-                numero = arquivo.stem.replace("Atividade", "")
-                novo_nome = pasta / f"Exemplo{numero}.c"
+                numero = arquivo.stem.replace("Exemplo", "")
+                novo_nome = pasta / f"Atividade{numero}.c"
                 
                 arquivo.rename(novo_nome)
                 print(f"Renomeado: {arquivo.name} -> {novo_nome.name}")
