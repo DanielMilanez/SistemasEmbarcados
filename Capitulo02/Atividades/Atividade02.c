9ea558ca1baf653cb8e9ced8640fd47fce851eef
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-01-21 15:36:25 -0300

    Removendo duplicatas

--- Capitulo02/Atividades/Atividade02.c
+++ Capitulo02/Atividades/Atividade02.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-    unsigned int dia, mes, ano;
-    
-    printf("DIA! MES! ANO!");
-    printf("\n---------------------------\n");
-    printf("Informe o ano o mes e o dia >> ");
-    scanf("%d %d %d", &ano, &mes, &dia);
-    printf("%d/%d/%d", dia, mes, ano);
+    short int minutos = 0;
+    float horas = 0.0;
+
+    printf("Insira os minutos para a conversao: ");
+    scanf("%hd", &minutos);
+
+    horas = (float) minutos / 60.0;
+    printf("\n%d min --> %.2fhrs ", minutos, horas);
 }
