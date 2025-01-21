223ab73af2514b8ea661612c4c702f0f86b601f5
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-01-21 15:44:56 -0300

    Adicionando explicação

--- Capitulo05/Conteudo/Exemplo02.c
+++ Capitulo05/Conteudo/Exemplo02.c
@@ -2,7 +2,7 @@
 
 // Criando um prótotipo
 void linha(int num); // Demonstra para o compilador o tipo de retorno da função
-
+/*O compilador se desvia do seu fluxo de resolução natural, declara esses pré processos e retrona para o flxo original*/
 int main(void){
     linha(3);
     linha(5);
