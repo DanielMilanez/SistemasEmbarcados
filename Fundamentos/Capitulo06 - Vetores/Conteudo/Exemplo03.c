c1d112914bc0deaf44e6573a6f1c2791354e4a09
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-01-22 17:07:32 -0300

    Criando código capaz de descobrir o valor de um vetor

--- Fundamentos/Capitulo06 - Vetores/Conteudo/Exemplo03.c
+++ Fundamentos/Capitulo06 - Vetores/Conteudo/Exemplo03.c
@@ -1,11 +1,9 @@
-// Matriz é uma extensão de vetores
 #include <stdio.h>
+#define len(arr) (int)(sizeof(arr) / sizeof((arr)[0])) // Lembre-se de fazer o casting
 
 int main(void){
-    int matriz[3][3] = {{1, 2, 3}, 
-                        {4, 5, 6}, 
-                        {7, 8, 9}};
-    
-
-    // Exercicio: Escreva as informações da matriz na tela
+    int myVetor[100] = {0};
+    printf("%d", len(myVetor));
 }
+
+// Isso também poderia ser implementado em uma função, sem falar que a utilização de macro é bem melhor
