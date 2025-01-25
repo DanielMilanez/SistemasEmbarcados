4a2a17dd14d2ef78ba4c868515341566a78ee8cf
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-01-24 21:21:05 -0300

    Atividade

--- Fundamentos/Capitulo08 - Pointers/Atividade/Atividade01.c
+++ Fundamentos/Capitulo08 - Pointers/Atividade/Atividade01.c
@@ -1,6 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-    
-}
+/*Projete uma pilha de memória em C que conhetnha 16 niveis, onde cada nivel irá armazenar um byte
+Para mostrar a pilha, os endereços e dados devem ser apresentados no formato hexadecimal.*/
